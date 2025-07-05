1065c2d36db854cff0d4afb42132ef4fc8d265c1
Silas Teague <silasteague@gmail.com>
2025-07-05 18:43:20 -0500

    Add movegen header

---
+++ src/movegen.h
@@ -0,0 +1,16 @@
+#ifndef MOVE_GEN_H
+#define MOVE_GEN_H
+
+#include "board.h"
+#include "move_list.h"
+
+void generate_all_moves(const Board board, MoveList* list);
+
+void generate_pawn_moves(const Board board, MoveList* list, int square);
+void generate_knight_moves(const Board board, MoveList* list, int square);
+void generate_bishop_moves(const Board board, MoveList* list, int square);
+void generate_rook_moves(const Board board, MoveList* list, int square);
+void generate_queen_moves(const Board board, MoveList* list, int square);
+void generate_king_moves(const Board board, MoveList* list, int square);
+
+#endif
