f7a63cbd2676cd3e5c90e67e55038f3d4d709939
Silas Teague <silasteague@gmail.com>
2025-07-05 13:21:02 -0500

    Add basic unmake_move() functionality

---
+++ src/unmake_move.c
@@ -0,0 +1,7 @@
+#include "unmake_move.h"
+
+void unmake_move(Board* board, const BoardDiff diff) {
+    board->squares[diff.move.from] = board->squares[diff.move.to];
+    board->squares[diff.move.to] = diff.captured_piece;
+    board->side_to_move = diff.side_to_move;
+}
---
+++ src/unmake_move.h
@@ -0,0 +1,9 @@
+#ifndef UNMAKE_MOVE_H
+#define UNMAKE_MOVE_H
+
+#include "board.h"
+#include "board_diff.h"
+
+void unmake_move(Board* board, const BoardDiff diff);
+
+#endif
