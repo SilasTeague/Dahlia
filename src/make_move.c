93374bff233763fc02810d927634deb2d2800e59
Silas Teague <silasteague@gmail.com>
2025-07-06 14:52:20 -0500

    Add en_passant_square handling to make_move.c

--- src/make_move.c
+++ src/make_move.c
@@ -1,10 +1,24 @@
 #include "make_move.h"
 
 void make_move(Board* board, Move move, BoardDiff* diff) {
-    // Populate BoardDiff diff
+    // Populate BoardDiff
     diff->move = move;
     diff->captured_piece = board->squares[move.to];
     diff->side_to_move = board->side_to_move;
+
+    Piece piece = board->squares[move.from];
+    if (piece == wP || piece == bP) {
+        int start_rank = (piece == wP) ? 1 : 6;
+        int direction = (piece == wP) ? 8 : -8;
+
+        if (move.from / 8 == start_rank && move.to == move.from + 2 * direction) {
+            board->en_passant_square = move.from + direction;
+        } else {
+            board->en_passant_square = -1;
+        }
+    } else {
+        board->en_passant_square = -1;
+    }
 
     if (move.promotion != NO_PROMOTION) {
         switch (move.promotion) {
