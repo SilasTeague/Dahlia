55faee47a326cfd2f2927082ffa09dfc19b04b20
Silas Teague <silasteague@gmail.com>
2025-07-14 20:12:52 -0500

    Implement perft divide

--- src/perft.c
+++ src/perft.c
@@ -3,6 +3,8 @@
 #include "board_diff.h"
 #include "make_move.h"
 #include "unmake_move.h"
+#include "move.h"
+#include <stdio.h>
 
 uint64_t perft(Board* board, int depth) {
     if (depth == 0) return 1;
@@ -22,4 +24,24 @@ uint64_t perft(Board* board, int depth) {
     }
 
     return nodes;
+}
+
+void perft_divide(Board* board, int depth) {
+    MoveList pl_moves, legal_moves;
+    generate_pseudo_legal_moves(board, &pl_moves);
+    generate_legal_moves(board, &pl_moves, &legal_moves);
+
+    int total = 0;
+    for (int i = 0; i < legal_moves.count; i++) {
+        Move move = legal_moves.moves[i];
+        BoardDiff diff;
+        make_move(board, move, &diff);
+        int count = perft(board, depth - 1);
+        unmake_move(board, diff);
+
+        print_move(move);
+        printf(": %d\n", count);
+        total += count;
+    }
+    printf("Total: %d\n", total);
 }
--- src/perft.h
+++ src/perft.h
@@ -5,5 +5,6 @@
 #include "board.h"
 
 uint64_t perft(Board* board, int depth);
+void perft_divide(Board* board, int depth);
 
 #endif
