bccd1cb508ed91434653182acbb52f5fea5af189
Silas Teague <silasteague@gmail.com>
2026-05-27 19:49:22 -0400

    Update Piece enum to accomodate bitboard layout

--- src/types.h
+++ src/types.h
@@ -3,20 +3,7 @@
 #include <cstdint>
 
 enum Piece : uint8_t {
-	EMPTY = 0,
-	WHITE_KING,
-	WHITE_QUEEN,
-	WHITE_ROOK,
-	WHITE_BISHOP,
-	WHITE_KNIGHT,
-	WHITE_PAWN,
-	BLACK_KING,
-	BLACK_QUEEN,
-	BLACK_ROOK,
-	BLACK_BISHOP,
-	BLACK_KNIGHT,
-	BLACK_PAWN,
-
+	PAWN = 0, KNIGHT, BISHOP, ROOK, QUEEN, KING
 };
 
 // We will start with a1 = 0, b1 = 1... such that
