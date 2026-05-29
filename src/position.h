68b154a12e10b122aa1ffd4701567e762fd222ed
Silas Teague <silasteague@gmail.com>
2026-05-28 20:04:56 -0400

    Create basic position struct

---
+++ src/position.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "bitboard.h"
+#include "types.h"
+
+
+
+struct Position {
+	Bitboard pieces[2][6];	// [color][piece]
+	Bitboard aggregates[3]; // [WHITE][BLACK[ALL] 
+
+	Color side_to_move;
+	uint8_t castling_rights;
+	Square en_passant_square;
+
+	uint8_t halfmove_clock;
+	uint16_t fullmove_count;
+}
